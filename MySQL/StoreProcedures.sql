f7050859041d25ebeedebef5493a676319646652
Franco <valterpassion@hotmail.com>
2018-06-24 16:51:46 -0300

    Update procs cliente

--- MySQL/StoreProcedures.sql
+++ MySQL/StoreProcedures.sql
@@ -7,12 +7,15 @@ CREATE PROCEDURE sp_cadastro_cliente (
     in ende_nome VARCHAR(100),
     in ende_numero INT(11),
     in end_complemento VARCHAR(100),
-    in ende_CEP varchar(10)
+    in ende_CEP varchar(10),
+	in end_bairro varchar(100),
+	in end_cidade varchar(100),
+	in end_uf varchar(2)
 )
 begin
 
-	insert into endereco (end_nome, numero, complemento, CEP)
-    values (ende_nome, ende_numero, end_complemento, ende_CEP);
+	insert into endereco (end_nome, numero, complemento, CEP, bairro, cidade, uf)
+    values (ende_nome, ende_numero, end_complemento, ende_CEP, end_bairro, edn_cidade, end_uf);
     
     insert into cliente (nome, CNPJ, endereco_end_id)
     values (cli_nome, cli_CNPJ, last_insert_id());
@@ -20,7 +23,7 @@ begin
 end$$
 DELIMITER ;
 
-call sp_cadastro_cliente('Lojão do Jão','tested','Rua Teste',44,'D','837283');
+call sp_cadastro_cliente('Lojão do Jão','tested','Rua Teste',44,'D','837283','Portão','Curitiba','PR');
 
 DELIMITER $$
 
@@ -32,7 +35,10 @@ CREATE PROCEDURE sp_atualizar_cliente (
     in ende_nome VARCHAR(100),
     in ende_numero INT(11),
     in end_complemento VARCHAR(100),
-    in ende_CEP varchar(10)
+    in ende_CEP varchar(10),
+	in end_bairro varchar(100),
+	in end_cidade varchar(100),
+	in end_uf varchar(2)
 )
 begin
 
@@ -45,7 +51,11 @@ begin
     set end_nome = ende_nome,
 		numero = ende_numero, 
         complemento = end_complemento,
-        CEP = ende_CEP where end_id = @teste;
+        CEP = ende_CEP,
+		bairro = end_bairro,
+		cidade = end_cidade,
+		uf = end_uf
+		where end_id = @teste;
     
     update cliente
     set cliente.nome = cli_nome,
